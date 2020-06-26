4cdcd1027afe4ae4086ce2bf48dbfa9cddfd5be1
kamia700 <kaana757@gmail.com>
2020-06-26 12:05:30 +0200

    pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,53 @@
+'use strict';
+(function () {
+  var StatusCode = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
+
+  var URL_POST = 'https://javascript.pages.academy/code-and-magick';
+
+  var URL_GET = 'https://javascript.pages.academy/code-and-magick/data';
+
+  var newXhr = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = newXhr(onLoad, onError);
+
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = newXhr(onLoad, onError);
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save,
+  };
+})();
